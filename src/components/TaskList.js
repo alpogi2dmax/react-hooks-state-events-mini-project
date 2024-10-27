eeaf17f62fb39a510df6333e4ac8c4a11669f440
Al Cerdan Lico <alcerdan@yahoo.com>
2024-10-26 20:52:18 -0700

    rendered tasks in tasklist

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({TASKS}) {
+
+  console.log({TASKS})
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {TASKS.map((task, index) => (
+        <Task key={index} text={task.text} category={task.category}/>
+      ))}
     </div>
   );
 }
